6982c2edb8817f791280029de2150c38173721ca
lambdaTW <lambda@lambda.tw>
2025-06-22 14:47:19 +0800

    feat: test new website

--- test_it.py
+++ test_it.py
@@ -1,14 +1,14 @@
+import re
 from playwright.sync_api import Page, expect
-from pytest_playwright.pytest_playwright import CreateContextCallback
 
 
 def test_search_google_image(page: Page) -> None:
-    page.goto("https://google.com")
-    page.locator("xpath=/html/body/div[1]/div[2]/div/img").hover()
+    page.goto("https://tw.yahoo.com")
+    page.locator('xpath=/html/body/div[1]/div/nav/div/div[1]/ul/li[1]').hover()
     page.mouse.down()
-    page.mouse.move(403, 391)
-    page.locator("xpath=/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div[2]/c-wiz/div/div/div[2]/div[2]").hover()
+    search_input = page.locator("xpath=/html/body/header/div/div/div[1]/div/div/div[1]/div[1]/div/div/form/input[1]")
+    box = search_input.bounding_box()
+    page.mouse.move(box["x"] + box["width"] / 2, box["y"] + box["height"] / 2)
+    search_input.hover()
     page.mouse.up()
-    expect(
-        page.locator('//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/div/div/div[2]/a/div/div[2]/div/div/span')
-    ).to_contain_text("google", ignore_case=True)
+    expect(search_input).to_have_value(re.compile("news"))
